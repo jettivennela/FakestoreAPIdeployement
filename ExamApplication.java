07eb3fe33f84b2774cf51416bc0c827d85b1e5f2
jettivennela <168246365+jettivennela@users.noreply.github.com>
2024-12-07 12:42:58 +0530

    Add files via upload

---
+++ ExamApplication.java
@@ -0,0 +1,12 @@
+package com.klef.jfsd.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ExamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExamApplication.class, args);
+    }
+}
